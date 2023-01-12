753a7ec12b48390e6973b2d91cf48a22f3b7d104
OsundeWatson <watsonosunde@gmail.com>
2023-01-12 13:52:21 +0100

    created an express server in server folder

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = rxpress(;
+    
+    
+    const port = 5000;
+    
+    app.listen(port, () =>
+       console.log(`server listening on port ${port)`)
+    ))
